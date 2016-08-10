0068662b48828bfe15e8398fca5216574ece44d6
Moriel Schottlender <moriel@gmail.com>
2016-08-10 00:17:06 -0700

    Use Twig for templating the front end

---
+++ bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'vendor/autoload.php';
+
+$twigLoader = new Twig_Loader_Filesystem( __DIR__ . '/templates');
+$twig = new Twig_Environment( $twigLoader, array(
+    'cache' => __DIR__ . '/cache',
+) );
+
+$baseurl = $currentPage === 'test' ? $_SERVER['REQUEST_URI'] : dirname( $_SERVER['REQUEST_URI'] );
+if ( strpos( $baseurl, '/' ) !== strlen( $baseurl ) - 1 ) {
+	// HACK: Add trailing slash only if one doesn't exist.
+	$baseurl .= '/';
+}
+
+$menuItems = array(
+	'test' => array(
+		'url' => $baseurl,
+		'label' => 'Test!'
+	),
+	'explanation' => array(
+		'url' => 'explanation.php',
+		'label' => 'Explanation',
+	),
+	'rtl.wtf' => array(
+		'url' => 'http://rtl.wtf',
+		'label' => 'rtl.wtf'
+	)
+);
--- index.php
+++ index.php
@@ -1,26 +1,13 @@
 <?php
+
 $currentPage = 'test';
-$scripts[] = 'assets/rtl.works.js';
-include_once 'html_parts/page_head.php'
-?>
-	<div class="inner cover">
-		<h1 class="cover-heading">Does my site work with Right-to-Left?<a href="explanation.php">*</a></h1>
-		<p></p>
-		<p class="lead centered">
 
-		<!-- FORM -->
-		<div class="input-group">
-			<input type="text" id="rtlworks-url-input" class="form-control" placeholder="Website...">
-			<span class="input-group-btn">
-				<button id="rtlworks-analyze-button" class="btn btn-primary" type="button">Analyze!</button>
-			</span>
-		</div><!-- /input-group -->
-		<!-- /FORM -->
-		</p>
+include 'bootstrap.php';
 
-		<div id="rtlworks-loading"></div>
-		<div id="rtlworks-result"></div>
-	</div>
-<?php
-include_once 'html_parts/page_foot.php'
-?>
+$menuItems[ $currentPage ][ 'active' ] = true;
+
+echo $twig->render( 'test.html', array(
+	"baseurl" => $baseurl,
+	"menu" => $menuItems,
+	"scripts" => array( 'assets/rtl.works.js' ),
+) );
