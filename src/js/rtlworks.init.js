0404cb8136bdd9b88c2f90efc4fd1d5939bacff2
Moriel Schottlender <moriel@gmail.com>
2016-08-13 14:08:50 -0700

    Allow submit on Enter keypress
    Fixes #15

--- src/js/rtlworks.init.js
+++ src/js/rtlworks.init.js
@@ -6,47 +6,56 @@
 			$resultDiv = $( '#rtlworks-result' ),
 			$loading = $( '#rtlworks-loading' )
 				.addClass( 'rtlworks-spinner' )
-				.hide();
+				.hide(),
+			onAnalyzeButtonClick = function onAnalyzeButtonClick () {
+				var url = $input.val();
 
-		$button.on( 'click', function () {
-			var url = $input.val();
-
-			$resultDiv
-				.slideUp()
-				.empty();
-
-			if ( !rtlworks.network.isUrlValid( url ) ) {
 				$resultDiv
-					.append(
-						$( '<div>' )
-							.addClass( 'alert alert-warning' )
-							.attr( 'role', 'alert' )
-							.text( 'Please provide a valid URL.' )
-						)
-						.slideDown();
+					.slideUp()
+					.empty();
+
+				if ( !rtlworks.network.isUrlValid( url ) ) {
+					$resultDiv
+						.append(
+							$( '<div>' )
+								.addClass( 'alert alert-warning' )
+								.attr( 'role', 'alert' )
+								.text( 'Please provide a valid URL.' )
+							)
+							.slideDown();
+					return false;
+				}
+
+				$loading.show();
+				rtlworks.util.setDisabled( $button, true );
+				rtlworks.util.setDisabled( $input, true );
+
+				rtlworks.network.runTests( url, 'all' )
+					.then( function ( results ) {
+						// Show result
+						var model = new rtlworks.dm.ResultsModel( results ),
+							panel = new rtlworks.ui.ResultsPanel( model );
+
+						$resultDiv
+							.append( panel.$element )
+							.slideDown();
+
+						$loading.hide();
+					} )
+					.then( function () {
+						rtlworks.util.setDisabled( $button, false );
+						rtlworks.util.setDisabled( $input, false );
+					} );
+
+				return false;
+			};
+
+		$button.on( 'click', onAnalyzeButtonClick );
+		$input.on( 'keypress', function ( e ) {
+			if ( e.which === 13 ) {
+				onAnalyzeButtonClick();
 				return false;
 			}
-
-			$loading.show();
-			rtlworks.util.setDisabled( $button, true );
-			rtlworks.util.setDisabled( $input, true );
-
-			rtlworks.network.runTests( url, 'all' )
-				.then( function ( results ) {
-					// Show result
-					var model = new rtlworks.dm.ResultsModel( results ),
-						panel = new rtlworks.ui.ResultsPanel( model );
-
-					$resultDiv
-						.append( panel.$element )
-						.slideDown();
-
-					$loading.hide();
-				} )
-				.then( function () {
-					rtlworks.util.setDisabled( $button, false );
-					rtlworks.util.setDisabled( $input, false );
-				} );
 		} );
 	} );
 } )( jQuery );
